609945eeb8e17b7982186a3257102a1fd0c006dc
lankaize <2728701189@qq.com>
2020-08-17 12:00:36 +0800

    页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,50 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index/Index'
+import Login from '@/components/Index/Login'
+import List from '@/components/Index/List'
+import Search from '@/components/Index/Search'
+import Perslnfo from '@/components/Index/Perslnfo'
+import Detail from '@/components/Index/Detail'
+import Reg from '@/components/Index/Reg'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Index',
+      component: Index
+    },
+
+    {
+      path: '/Reg',
+      name: 'Reg',
+      component: Reg
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/List',
+      name: 'List',
+      component: List
+    },
+    {
+      path: '/Detail',
+      name: 'Detail',
+      component: Detail
+    },
+    {
+      path: '/Search',
+      name: 'Search',
+      component: Search
+    },
+    {
+      path: '/Perslnfo',
+      name: 'Perslnfo',
+      component: Perslnfo
+    },
   ]
 })
